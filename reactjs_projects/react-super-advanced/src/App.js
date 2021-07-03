4a0f710c79dc5e9932d0b8b227d682eb3b95b327
adityaankul <ankuladitya17@gmail.com>
2021-07-03 18:05:58 +0530

    react files

---
+++ reactjs_projects/react-super-advanced/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/1.functional/Greet'
+
+
+function App() {
+  return (
+    <div className="container" >
+    
+     <h1>Welcome to react development</h1>
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
